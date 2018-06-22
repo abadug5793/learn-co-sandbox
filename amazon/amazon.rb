4704fb94814eb2a28b84d25602c486acae407e6c
abadug5793 <40362281+abadug5793@users.noreply.github.com>
2018-06-22 08:59:51 -0400

    Update amazon.rb

--- amazon/amazon.rb
+++ amazon/amazon.rb
@@ -1,3 +1,4 @@
 food = {"dairy" => 3, "meats" => 10, "carbs" => 8, "drinks" => 3}
 toys = {"books" => 5, "dolls" => 10, "cars" => 20}
 electronics = {"laptop" => 80, "tablets" => 50, "headphones" => 15, "phones" => 100}
+clothing = {"shirts" => 10, "pants" => 10, "shoes" => 15}
